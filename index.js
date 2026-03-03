e73e3bd8b50be5366d038f4760a8de1612a5635c
blackbionic298 <blackbionic298@gmail.com>
2026-03-03 20:21:22 +0100

    index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const CONFIG = {
   checkTimeoutInterval: 300000      // 延长到 5 分钟
 };
 
-const BOT_USERNAME = 'LiveShowBot_lol';
+const BOT_USERNAME = '24hOnlineLiveChatBot';
 const AUTHME_PASSWORD = process.env.AUTHME_PASSWORD || 'deutschland';
 
 let bot;
